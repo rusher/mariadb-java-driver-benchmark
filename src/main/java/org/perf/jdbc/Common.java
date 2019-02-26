75438a1fee02b74df79d821a56b3da28224bb5bb
rusher <diego.dupin@gmail.com>
2019-02-26 11:17:17 -0500

    [misc] changing test to use QPS results

--- src/main/java/org/perf/jdbc/Common.java
+++ src/main/java/org/perf/jdbc/Common.java
@@ -26,8 +26,8 @@ import org.openjdk.jmh.annotations.Warmup;
 @Fork(value = 20)
 //not setting thread = number of processor
 //@Threads(value = 1)
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.SECONDS)
 public class Common {
 
   @State(Scope.Benchmark)
